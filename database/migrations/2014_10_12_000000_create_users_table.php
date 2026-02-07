9b446a55a8753b26caa6949242d2810197c2d567
Artenlaclase <artenlaclase@gmail.com>
2026-02-07 17:43:59 -0300

    Update composer dependencies and add JWT configuration file; extend users table with additional fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('age')->nullable();
+            $table->string('gender', 10)->nullable();
+            $table->decimal('weight', 5, 2)->nullable();
+            $table->decimal('height', 5, 2)->nullable();
+            $table->string('activity_level')->nullable();
+            $table->string('hta_level')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
